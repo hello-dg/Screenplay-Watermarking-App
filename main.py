e711ecb64f25cecb9c7f5ca0fc092c8398ea53dc
David Green <hellodg.py@gmail.com>
2024-08-12 21:44:57 -0700

    Only allow PDFs to be uploaded

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import tkinter as tk
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageDraw, ImageFont
+from PyPDF2 import PdfReader, PdfWriter
 
 
 # Function to handle button click
 def on_button_click():
     print("Button Works")
+
+
+def upload_file():
+    permitted_file_types = [("PDF files", '*.pdf')]
+    file_path = filedialog.askopenfilename(filetypes=permitted_file_types)
+    if file_path:
+        print(f"File selected: {file_path}")
 
 
 # Create the main window
@@ -17,7 +27,7 @@ root.iconbitmap('assets/icons/drop.ico')
 label = tk.Label(root, text="If you haven't already... Watermark It!", font=('Rockwell', 14), fg='#7393B3', bg='#fefefe')
 label.pack(pady=10)
 
-button = tk.Button(root, text="Upload Document", font=('Rockwell', 12), fg="#ffffff", bg='#7393B3', command=on_button_click)
+button = tk.Button(root, text="Upload Document", font=('Rockwell', 12), fg="#ffffff", bg='#7393B3', command=upload_file)
 button.pack(pady=10)
 
 label = tk.Label(root, text="Watermark Text:", font=('Rockwell', 12), fg='#7393B3', bg='#fefefe')
