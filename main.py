bc9f11721504d8a1ef19222eb52abd304e3437c4
David Green <hellodg.py@gmail.com>
2024-08-12 22:05:33 -0700

    Work on text generation for watermarking.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from tkinter import filedialog, messagebox
 from PIL import Image, ImageDraw, ImageFont
 from PyPDF2 import PdfReader, PdfWriter
 
+# Global Variables
+file_path = 'yes'
+
 
 # Function to handle button click
 def on_button_click():
@@ -10,10 +13,21 @@ def on_button_click():
 
 
 def upload_file():
+    global file_path
     permitted_file_types = [("PDF files", '*.pdf')]
     file_path = filedialog.askopenfilename(filetypes=permitted_file_types)
     if file_path:
         print(f"File selected: {file_path}")
+
+
+def generate_text_for_watermarking(text, font_size=36):
+    font = ImageFont.truetype('arial.ttf', font_size)
+    text_size = font_size
+    background = Image.new('RGBA', text_size, (255, 255, 255, 0))
+    draw = ImageDraw.Draw(background)
+    draw.text((0, 0), text, font=font, fill=(0, 0, 0, 128))
+
+    return background
 
 
 # Create the main window
@@ -27,14 +41,14 @@ root.iconbitmap('assets/icons/drop.ico')
 label = tk.Label(root, text="If you haven't already... Watermark It!", font=('Rockwell', 14), fg='#7393B3', bg='#fefefe')
 label.pack(pady=10)
 
-button = tk.Button(root, text="Upload Document", font=('Rockwell', 12), fg="#ffffff", bg='#7393B3', command=upload_file)
+button = tk.Button(root, text="Upload PDF Document", font=('Rockwell', 12), fg="#ffffff", bg='#7393B3', command=upload_file)
 button.pack(pady=10)
 
 label = tk.Label(root, text="Watermark Text:", font=('Rockwell', 12), fg='#7393B3', bg='#fefefe')
 label.pack(pady=10)
 
-entry = tk.Entry(root)
-entry.pack(pady=10)
+watermark_entry = tk.Entry(root)
+watermark_entry.pack(pady=10)
 
 button = tk.Button(root, text="Watermark It!", font=('Rockwell', 12), fg="#ffffff", bg='#7393B3', command=on_button_click)
 button.pack(pady=10)
